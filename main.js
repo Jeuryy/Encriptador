adc8a41bd0c2631a26a119097676a6181ee6fc46
Jeury Pierre Dide <elyruej.102004@gmail.com>
2023-01-07 23:07:52 -0400

    Only missing copying button!!!

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
     // Variables
-    var encryptBtn = document.getElementById("btn-encrypt");
-    var decryptBtn = document.getElementById("btn-decrypt");
     var showText = document.getElementById("notfound-text");
     var copyBtn = document.getElementById("copy");
     var clearBtn = document.getElementById("clear-content");
     var notFoundImg = document.getElementById("notfound-img");
     var textToEncrypt;
+    var textToCopy;
 
 
     function hideElements() {
@@ -20,6 +19,9 @@
         copyBtn.style.display = "initial";
         clearBtn.style.display = "initial";
     }
+    const removeAccents = (str) => {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
 
     /*La letra "e" es convertida para "enter"
       La letra "i" es convertida para "imes"
@@ -28,12 +30,14 @@
       La letra "u" es convertida para "ufat"*/
     function encrypt() {
         textToEncrypt = document.getElementById("text").value;
+        textToEncrypt = removeAccents(textToEncrypt);
         textToEncrypt = textToEncrypt.replace(/a/igm, "ai");
         textToEncrypt = textToEncrypt.replace(/e/igm, "enter");
         textToEncrypt = textToEncrypt.replace(/i/igm, "imes");
         textToEncrypt = textToEncrypt.replace(/o/igm, "ober");
         textToEncrypt = textToEncrypt.replace(/u/igm, "ufat");
         showText.innerHTML = textToEncrypt.toLowerCase();
+        //copyBtn.onclick = copyText(showText.innerHTML);
 
         if (textToEncrypt == "") {
             hideElements();
@@ -44,12 +48,14 @@
 
     function decrypt() {
         textToEncrypt = document.getElementById("text").value;
-        textToEncrypt = textToEncrypt.replace(/ai/igm, "a");
+        textToEncrypt = removeAccents(textToEncrypt);
+        textToEncrypt = textToEncrypt.replace(/aimes/igm, "a");
         textToEncrypt = textToEncrypt.replace(/enter/igm, "e");
         textToEncrypt = textToEncrypt.replace(/imes/igm, "i");
         textToEncrypt = textToEncrypt.replace(/ober/igm, "o");
         textToEncrypt = textToEncrypt.replace(/ufat/igm, "u");
         showText.innerHTML = textToEncrypt.toLowerCase();
+        //copyBtn.onclick = copyText(showText.innerHTML);
 
         if (textToEncrypt == "") {
             hideElements();
@@ -58,9 +64,20 @@
         }
     }
 
+    function copyText(text) {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                alert("Copied");
+            })
+            .catch(err => {
+                alert("Error, err");
+            })
+    }
+
     function clearText() {
         textToEncrypt = document.getElementById("text").value = "";
         hideElements();
     }
+
 
     window.onload = hideElements;
